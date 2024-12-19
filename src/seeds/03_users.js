05ac8f4538e69f8fc5fa70579fc5ff5b45753f81
JEFFERSON STARLING BARRERA CARDONA <jefferson.barrera@konecta-group.com>
2024-12-19 09:51:48 -0500

    @responsable jefferson starling barrera cardona primer commit prueba node

--- src/seeds/03_users.js
+++ src/seeds/03_users.js
@@ -2,7 +2,7 @@ export  function seed(knex) {
     return knex('users').del()
       .then(function () {
         return knex('users').insert([
-          { email: 'taling1993@gmail.com', password: '123456' }
+          { email: 'prueba@prueba.com', password: '123456' }
         ]);
       });
   };
